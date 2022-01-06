8c045d2736138542453d2300e3a681a108372e8e
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-01-06 13:33:37 -0800

    remove strict mode test

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Router>
       <App />
-    </Router>
-  </React.StrictMode>,
+    </Router>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
