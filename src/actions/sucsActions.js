1dfa9ccbc0f96df042334fc5bd6b2f4f2be183a7
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-01-07 14:51:53 -0800

    fixed base url bug - not hidden in deploy

--- src/actions/sucsActions.js
+++ src/actions/sucsActions.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
+import { BASE_URL } from '../constants'
+
 export const FETCH_START = 'FETCH_START'
 export const FETCH_SUCCESS = 'FETCH_SUCCESS'
 export const FETCH_ERROR = 'FETCH_ERROR'
-
 
 export const fetchStart = () => {
     return({type:FETCH_START})
@@ -21,19 +22,14 @@ export const getSucs = () => {
     return(dispatch) => {
         dispatch(fetchStart())
 
-        // what is going on with BASE_URL ... axios get works with deployed backend url
-        // might have to add http:// to BASE_URL
         axios.get(
-            (process.env.BASE_URL ? `${process.env.BASE_URL}/api/sucs` : 
-            `http://${process.env.REACT_APP_BASE_URL}/api/sucs`)
-            // || `http://${process.env.REACT_APP_BASE_URL_DEV}/api/sucs`
+            // toggle `${http://localhost:9000/` for development
+            `${BASE_URL}/api/sucs`
             )
             .then(res => {
                 dispatch(fetchSuccess(res.data))
             })
             .catch(err => {
-                console.log(process.env.BASE_URL)
-                console.log(process.env.REACT_APP_BASE_URL)
                 dispatch(fetchError(err))
             })
     }
