c1402a69b31a69d18edc470a8d9ecdd2f0d5d1af
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-01-06 18:22:40 -0800

    links on nav header working

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Sucs and Sched
 ## Frontend App for simple fitness schedules
+
+
+Run Cards-
+find icons
+race website
+px more on lg than rest
+
+
+SUCS - when logged in, user can see progress of other users and toggle complete for any day
+
--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
@@ -6,54 +6,65 @@ import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 
 import { LinkContainer } from 'react-router-bootstrap/';
+import { useNavigate } from 'react-router-dom'
 
 import logo from '../assets/logo.ico'
 
 function NavComponent(props) {
+    
+    const navigate = useNavigate()
+
+    const spanClick = (where) => {
+        navigate(where)
+    }
 
     return (
 
-<Navbar bg="success" expand="false" className='py-0 my-0 sticky-top'>
-  <Container className='d-flex px-lg-0' >
-    <Navbar.Brand className='p-0 mb-1'><img src={logo} alt='logo'/></Navbar.Brand>
-    
-    {/* figure out how to make sucs and sched links */}
-    <h2 className='p-0 pt-1'>sucs and sched</h2>
-    
-    <Navbar.Toggle className='ms-auto p-0'/>
-
-    {/* figure out how to close offcanvas onclick */}
-        <Navbar.Offcanvas
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-            placement="end"
-            className='w-auto p-1 d-flex'
-        > 
-            <LinkContainer to='/'>
-                <Nav.Link>Home</Nav.Link>
-            </LinkContainer>
-
-            <LinkContainer to='/account'>
-                <Nav.Link>Account</Nav.Link>
-            </LinkContainer>
+    <Navbar bg="success" expand="false" className='py-0 my-0 sticky-top'>
+        <Container className='d-flex px-lg-0' >
+            <Navbar.Brand className='p-0 mb-1'><img src={logo} alt='logo'/></Navbar.Brand>
             
-            <LinkContainer to='/sucs'>
-                <Nav.Link>SUCS</Nav.Link>
-            </LinkContainer>
+            {/* figure out how to make sucs and sched links */}
+            <h2 className='p-0 pt-1'>
+                <span onClick={() => spanClick('/sucs')}>sucs</span> 
+                <span> and </span>
+                <span onClick={() => spanClick('/sched')}>sched</span>
+            </h2>
             
-            <LinkContainer to='/sched'>
-                <Nav.Link>Sched</Nav.Link>
-            </LinkContainer>
+            <Navbar.Toggle className='ms-auto p-0'/>
 
-            <NavDropdown.Divider />
-            <LinkContainer to='/logout'>
-                <Nav.Link>Logout</Nav.Link>
-            </LinkContainer>
+            {/* figure out how to close offcanvas onclick */}
+                <Navbar.Offcanvas
+                    id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel"
+                    placement="end"
+                    className='w-auto p-1 d-flex'
+                > 
+                    <LinkContainer to='/'>
+                        <Nav.Link>Home</Nav.Link>
+                    </LinkContainer>
 
-        </Navbar.Offcanvas>
-  </Container>
-</Navbar>
-       
+                    <LinkContainer to='/account'>
+                        <Nav.Link>Account</Nav.Link>
+                    </LinkContainer>
+                    
+                    <LinkContainer to='/sucs'>
+                        <Nav.Link>SUCS</Nav.Link>
+                    </LinkContainer>
+                    
+                    <LinkContainer to='/sched'>
+                        <Nav.Link>Sched</Nav.Link>
+                    </LinkContainer>
+
+                    <NavDropdown.Divider />
+                    <LinkContainer to='/logout'>
+                        <Nav.Link>Logout</Nav.Link>
+                    </LinkContainer>
+
+                </Navbar.Offcanvas>
+        </Container>
+    </Navbar>
+            
     )
 }
 
