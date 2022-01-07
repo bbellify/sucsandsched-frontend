118e363ff131cc9503c26c9d7f2768058f97e126
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-01-06 22:31:44 -0800

    added western states

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 ### To Do
 1. Auth/login
     - Sucs - when logged in, users can see progress of all users and complete days for themselves 
+
+2. Close Offcanvas on link click
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import river from '../assets/river.png'
 import mountain from '../assets/mountain.png'
+import western from '../assets/western.png'
 
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
@@ -15,7 +16,7 @@ function Home() {
         <Container>
             <h2 className='my-3'>Upcoming Races:</h2>
             <Container className='px-0 d-md-flex justify-content-md' >
-                <Card className='shadow-sm mb-4 col-md-5 col-lg-4 me-md-3'>
+                <Card className='shadow-sm mb-4 col-md-4 col-lg-4 me-md-3'>
                     <Card.Img variant="top" src={river} className='p-4 pb-2' />
                     <Card.Body className='my-0'>
                         <Card.Title>Silver Falls Trail Challenge</Card.Title>
@@ -25,13 +26,23 @@ function Home() {
                     </Card.Body>
                 </Card>
 
-                <Card className='shadow-sm mb-4 col-md-5 col-lg-4 me-md-3'>
+                <Card className='shadow-sm mb-4 col-md-4 col-lg-4 me-md-3'>
                     <Card.Img variant="top" src={mountain} className='p-4 pb-2' />
                     <Card.Body className='my-0'>
                         <Card.Title>Three Sisters Skyline</Card.Title>
                         <Card.Subtitle className='text-muted py-1'>50k</Card.Subtitle>
                         <Card.Subtitle className='text-muted py-1'>September 24, 2022</Card.Subtitle>
                         <Card.Link href='https://www.alpinerunning.co/three-sisters-skyline' target='_blank'>website</Card.Link>
+                    </Card.Body>
+                </Card>
+
+                <Card className='shadow-sm mb-4 col-md-4 col-lg-4 me-md-3'>
+                    <Card.Img variant="top" src={western} className='p-4 mb-1' />
+                    <Card.Body className='my-0 mt-2'>
+                        <Card.Title>Western States</Card.Title>
+                        <Card.Subtitle className='text-muted py-1'>100m</Card.Subtitle>
+                        <Card.Subtitle className='text-muted py-1'>TBD</Card.Subtitle>
+                        <Card.Link href='https://www.wser.org/' target='_blank'>website</Card.Link>
                     </Card.Body>
                 </Card>
 
@@ -45,6 +56,7 @@ function Home() {
                         <ListGroupItem>River by Dong Ik Seo from NounProject.com</ListGroupItem>
                         <ListGroupItem>Mountain by Florent B from NounProject.com
                         </ListGroupItem>
+                        <ListGroupItem>Vertex by Marina Pugacheva from NounProject.com</ListGroupItem>
                     </ListGroup>
                 </Card>
             
