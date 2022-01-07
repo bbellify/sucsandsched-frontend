6dbf2f36e1ccc4b98e097af935fec94d19f3926a
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-01-07 15:55:09 -0800

    log in under construction - focus shifting to account

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
+
+import { BASE_URL } from '../constants/index'
 
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
@@ -23,9 +25,14 @@ function Login(props) {
         e.preventDefault()
         console.log('submit')
 
-        // axios.post('url', formValues)
-        //     .then()
-        //     .catch()
+        axios.post(`${BASE_URL}/auth/login`, formValues)
+            .then(res => {
+                console.log(res)
+                localStorage.setItem('token', res.data.token)
+                navigate('/account')
+                // going to have to dispatch a users action I think
+            })
+            .catch()
     }
 
     //temporary - remove this 
@@ -34,43 +41,45 @@ function Login(props) {
     })
 
     return (
-        <Container className='mt-4 text-center'>
+        <Container className='mt-4 d-flex flex-column align-items-center col-md-6'>
             <form 
                 onSubmit={handleSubmit}
-                className='d-flex flex-column align-items-center'
+                className='text-center w-75'
+
             >
-                <div className='w-75 form-group'>
-                    <h3>welcome back</h3>
-                    <input
-                        name='username'
-                        type='text'
-                        placeholder='username'
-                        className='form-control my-1'
-                        value={formValues.username}
-                        onChange={handleChange}
-                    ></input>
-                    <input 
-                        name='password'
-                        // remove comment below when live
-                        // type='password'
-                        placeholder='password'
-                        className='form-control my-1'
-                        value={formValues.password}
-                        onChange={handleChange}
-                    ></input>
-                    <Button 
-                        className='w-100 w-md-50 my-1'
-                        variant='light'
-                        type='submit'
-                    >
-                        login
+                <h3>welcome back</h3>
+                <input
+                    name='username'
+                    type='text'
+                    placeholder='username'
+                    className='form-control my-1'
+                    value={formValues.username}
+                    onChange={handleChange}
+                ></input>
+                <input 
+                    name='password'
+                    remove comment below when live
+                    type='password'
+                    placeholder='password'
+                    className='form-control my-1'
+                    value={formValues.password}
+                    onChange={handleChange}
+                ></input>
+                
+                {/* alert goes here */}
+
+                <Button 
+                    className='w-100 w-md-50 my-1'
+                    variant='light'
+                    type='submit'
+                >
+                    login
+                </Button>
+                <LinkContainer className='w-100' to='/register'>
+                    <Button variant='light'>
+                        need an account?
                     </Button>
-                    <LinkContainer className='w-100' to='/register'>
-                        <Button variant='light'>
-                            need an account?
-                        </Button>
-                    </LinkContainer>
-                </div>
+                </LinkContainer>
             </form>
         </Container>
     );
